bb6d89343c488aa40091e03e8de3df9edd96bc8d
Florian Fittschen <ffittschen@gmail.com>
2018-10-29 09:57:41 +0100

    Add netlify redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     copyright: `© ${(new Date()).getFullYear()} All rights reserved.`,
     menu: [
       {
-        label: 'Blog',
+        label: 'Articles',
         path: '/',
       },
       {
