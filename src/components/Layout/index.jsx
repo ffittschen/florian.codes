2f35cbccf93af047e0c31620d9157fb8b67c45fd
Florian Fittschen <ffittschen@gmail.com>
2018-10-29 17:16:53 +0100

    Add favicons

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -8,7 +8,18 @@ class Layout extends React.Component {
 
     return (
       <div className="layout">
-        <Helmet defaultTitle="Blog by John Doe" />
+        <Helmet defaultTitle="Florian Fittschen">
+          {/* Favicon stuff from realfavicongenerator.net */}
+          <meta name="apple-mobile-web-app-title" content="florian.codes" />
+          <meta name="application-name" content="florian.codes" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff8126" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Helmet>
         {children}
       </div>
     )
